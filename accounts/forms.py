1a823d12e681b7e409ece7afe2cb5e07167e9e1f
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2021-08-25 21:44:34 -0700

    ran black and isort

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,17 +1,18 @@
 # accounts/forms.py
 
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
 from .models import CustomUser
 
-class CustomUserCreationForm(UserCreationForm):
 
+class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ('username', 'email')
+        fields = ("username", "email")
+
 
 class CustomUserChangeForm(UserChangeForm):
-
     class Meta:
         model = CustomUser
-        fields = ('username', 'email')
+        fields = ("username", "email")
--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class CustomUser(AbstractUser):
 
+class CustomUser(AbstractUser):
     def __str__(self):
         return self.email
