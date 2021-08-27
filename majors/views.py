189c107dbe766627db5d49679fb5052b11bde1d1
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2021-08-27 09:58:49 -0700

    working

--- majors/views.py
+++ majors/views.py
@@ -9,7 +9,6 @@ class MajorDetailView(DetailView):
     model = Major
     template_name = "major_detail.html"
 
-
 class MajorListView(ListView):
     model = Major
     template_name = "major_list.html"
