e5d8decb0d1e9b577d8c6207ef0ff20a65020621
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2021-08-29 21:53:28 -0700

    add CourseType model

--- courses/admin.py
+++ courses/admin.py
@@ -2,6 +2,7 @@
 
 from django.contrib import admin
 
-from .models import Course
+from .models import Course, CourseType
 
 admin.site.register(Course)
+admin.site.register(CourseType)
--- courses/models.py
+++ courses/models.py
@@ -7,11 +7,29 @@ from django.urls import reverse
 from colleges.models import College
 
 
+class CourseType(models.Model):
+    name = models.CharField(max_length=30)
+    order = models.IntegerField(unique=True)
+    description = models.TextField(max_length=500, blank=True)
+
+    class Meta:
+        ordering = ["order"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def get_absolute_url(self):
+        return reverse("course_type_detail", args=[str(self.id)])
+
+
 class Course(models.Model):
     number = models.CharField(max_length=10)
     name = models.CharField(max_length=50)
     credits = models.FloatField(null=True, blank=True)
     college = models.ForeignKey(College, on_delete=models.CASCADE)
+    course_type = models.ForeignKey(
+        CourseType, null=True, blank=True, on_delete=models.CASCADE
+    )
     pre_reqs = models.CharField(max_length=50, blank=True)
     description = models.TextField(max_length=500, blank=True)
     course_outcomes = models.TextField(max_length=500, blank=True)
